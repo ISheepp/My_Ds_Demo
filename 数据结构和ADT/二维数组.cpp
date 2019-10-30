5ca96ec166d142a7c05c561077430ca0fed6d2ff
linziyang1106 <810230854@qq.com>
2019-10-30 22:28:37 +0800

    二维数组

---
+++ 数据结构和ADT/二维数组.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int NUM_ROWS = 30,
+              NUM_COLUMNS = 20;
+    //使用typedef声明一个通用的函数
+    typedef double TwoDimArray[NUM_ROWS][NUM_COLUMNS];
+    TwoDimArray scoresTable;
+    int numStudents, numTests;
+    cout << "# students and # of tests? ";
+    cin >> numStudents >> numTests;
+
+    cout <<"Enter" << numTests << " test scores for student\n";
+    for(int r = 0; r < numStudents; r++) //外循环控制行
+    {
+        cout << '#' << r+1 << ':';
+        for(int c = 0; c <numTests; c++) //内循环控制列
+        cin >> scoresTable[r][c];
+    } 
+}
